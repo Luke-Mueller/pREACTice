2c0834576d49dc73b4d6949b94fb3ee146acbf9a
Luke Mueller <muelll79@hotmail.com>
2020-02-11 10:20:10 -0700

    file cleanup

--- src/components/x/x2/x2.js
+++ src/components/x/x2/x2.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 import './x2.css';
 
-const X2 = props => {
+const X2 = () => {
   const clickHandler = e => {
     const className = e.target.className;
-    console.log(className);
     if (className === "X2_Container") {
       e.target.className = "X2_Container active";
     } else {
       e.target.className = "X2_Container";
     }
-
   };
 
   return (
