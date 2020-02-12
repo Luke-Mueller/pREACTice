52a692bc963e4b9d16418f77b21d36468dc3cfa5
Luke Mueller <muelll79@hotmail.com>
2020-02-11 17:01:21 -0700

    x1 animation

--- src/components/x/x1/x1.js
+++ src/components/x/x1/x1.js
@@ -3,22 +3,20 @@ import React from 'react';
 import './x1.css';
 
 const X1 = () => {
-
   const clickHandler = e => {
     const className = e.target.className;
-    console.log(className);
-    if (className === "X_Container") {
-      e.target.className = "X_Container active";
+    if (className === "X1_Container") {
+      e.target.className = "X1_Container active";
     } else {
-      e.target.className = "X_Container";
+      e.target.className = "X1_Container";
     }
   };
 
   return (
-    <div className="X_Container" onClick={clickHandler}>
-      {/* <div className="X_Div"></div> */}
-      {/* <div className="X_Div"></div> */}
-      <div className="X_Div"></div>
+    <div className="X1_Container" onClick={clickHandler}>
+      <div className="X1_Div"></div>
+      <div className="X1_Div"></div>
+      <div className="X1_Div"></div>
     </div>
   )
 };
